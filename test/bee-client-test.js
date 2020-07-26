d4ebae98affcc9bebe4549ea4bb6a6268fcb4429
wendydv1989 <mikethuy@gmail.com>
2020-07-27 00:08:44 +0200

    better testing

--- test/bee-client-test.js
+++ test/bee-client-test.js
@@ -3,11 +3,27 @@ const spies = require('chai-spies')
 const util = require('util')
 const fs = require('fs')
 const swarm = require('swarm-lowlevel')
+const { toHex, hexToByteArray, byteArrayToHex, numbersToByteArray, stringToUint8Array } = require('./conversion')
+const utf8 = require('utf8-encoder')
+const base32Encode = require('base32-encode')
+const base32Decode = require('base32-decode')
+const base32Variant = 'Crockford'
+const textEncoding = require('text-encoding')
+var TextDecoder = textEncoding.TextDecoder
+var TextEncoder = textEncoding.TextEncoder
+
+const td = new TextDecoder("utf-8")
+const te = new TextEncoder("utf-8")
+//const encodeId = (buffer) => base32Encode(buffer, base32Variant)
+// const decodeId = (id) => {
+//     // console.log('decodeId', {id})
+//     return new Uint8Array(base32Decode(id, base32Variant))
+// }
 
 var path = require('path');
 
 const BeeClient = require('../src/bee-client');
-const { assert } = require('console');
+const assert = require('chai').assert
 
 const readFileAsync = util.promisify(fs.readFile)
 
@@ -22,25 +38,22 @@ function delay(milliseconds) {
 
 const wallet = new swarm.unsafeWallet();
 
-const toHex = byteArray => Array.from(byteArray, (byte) => ('0' + (byte & 0xFF).toString(16)).slice(-2)).join('')
-
 let tempHash = ''
 
 const userObject = {
-    avatar: "",
-    username: "Berlin Club Yay",
+    avatar: "data",
+    username: "Boys Club Berlin",
     status: "accountCreated"
 }
 
-const data = new Uint8Array([0x66, 0x6f, 0x6f]);
+const rawTopic = te.encode("userdata");
+const uint8 = new Uint8Array(32);
+uint8.set(rawTopic, 0)
+const topic = uint8
 
-
-const file = "";
+const data = te.encode(JSON.stringify(userObject))
 
 const bee = new BeeClient("http://localhost:8080/chunks", null)
-
-const topic = new Uint8Array(32) // Say i want to do "userdata"
-
 
 describe('BeeClient', () => {
     describe('Testing', () => {
@@ -55,42 +68,27 @@ describe('BeeClient', () => {
             const bee = new BeeClient("http://localhost:8080/chunks", null)
             const hash = await bee.downloadData(tempHash).then(file => {
                 file = file
-
             })
-            assert(data, file, "Stored is not the same as retrieved")
+            assert.equal(data, file, "Stored is not the same as retrieved")
         })
         it('creates a feed', async () => {
-             const res = await bee.addFeed(wallet)
-             const res2 = await bee.updateFeed(data, wallet)
+            const res = await bee.addFeed(wallet)
+            const res2 = await bee.updateFeed(data, wallet)
         })
         it('reads a feed', async () => {
             const res = await bee.getFeed(wallet)
-            //console.log(res)
-            //const res2 = await bee.updateFeed("Hello", wallet)
-       })
-       it('creates a feed w topic', async () => {
-           console.log("set topic: ",topic)
-        const res = await bee.addFeedWithTopic(topic, wallet)
-        const res2 = await bee.updateFeedWithTopic(topic, data, wallet)
-   })
-   it('reads a feed w topic', async () => {
-       const res = await bee.getFeedWithTopic(topic, wallet)
-       console.log(res)
-       //const res2 = await bee.updateFeed("Hello", wallet)
-  })
-       
-        // it(' userobject', async () => {
-        //     // Save the user object to SOC and upload it to bee
-        //     const data = JSON.parse(userObject)
-        //     const bee = new BeeClient("http://localhost:8080/chunks", null)
-        //     //assert(data, file, "Stored is not the same as retrieved")
-        // })
-        // it('get latest userobject', async () => {
-        //     // Retrieve the user object to SOC 
-        //     const user = JSON.parse(result)
-        //     //assert(data, file, "Stored is not the same as retrieved")
-        // })
+            const string = td.decode(res.chunk.data)
+            assert.equal(string, JSON.stringify(userObject), 'userObject is not found')
+        })
+        it('creates a feed w topic', async () => {
+            const res = await bee.addFeedWithTopic(topic, wallet)
+            const res2 = await bee.updateFeedWithTopic(topic, data, wallet)
 
-        
+        })
+        it('reads a feed w topic', async () => {
+            const res = await bee.getFeedWithTopic(topic, wallet)
+            var string = td.decode(res.chunk.data);
+            assert.equal(string, JSON.stringify(userObject), 'userObject is not found')
+        })
     })
 })
